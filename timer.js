cf51b2fb687b9c43bd350aac877ec1034ea7412f
Rohit Kaulgud <rohit1k9@yahoo.com>
2021-03-14 09:44:44 +0530

    update countdown

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 24, 2021 23:00:00").getTime();
+var countDownDate = new Date("Mar 14, 2021 23:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
